4d37f28323555c97b6bcc1df54f9e114968ca75d
nono tize kod <djcase001@gmail.com>
2016-06-29 00:38:24 -0400

    Adds an up to date dsm.js module. This module has more interactive feature

--- js/dsm.js
+++ js/dsm.js
@@ -1,26 +1,51 @@
 var nono = (function(){
 
-	var tags = /\b(div|aside|section|article|span|a|ul|li|ol|header|footer|h1|h2|h3|h4|h5|h6|img|p|button|input|select|label|table|theader|tbody|td|tr)\b/;
-	var attributes = /\b(color|background-color|font-size)\b/;
+	var tags = /\b(?:div|aside|section|article|span|a|ul|li|ol|header|footer|h1|h2|h3|h4|h5|h6|img|p|button|input|select|label|table|thead|tbody|th|td|tr|body|caption)\b/;
+	var attributes = /\b(?:color|background-color|font-size|text-decoration|border|font-style|width)\b/;
+	var embeds = /\b(?:src|id|class|href)\b/;
 	var recup = null;
 
 	function selection(arg){
 		if(typeof arg === 'string'){
-			if(arg[0] === '.'){
+			if(arg[0] === '.'){  // If the selection is by string of a specific class
 				arg = arg.substr(1);
 				recup = document.getElementsByClassName(arg);
-				return this;
-			}else if(arg[0] === '#'){
+				if(recup.length === 1){ // if there's only one element selected
+					recup = recup[0];
+				}else if(recup.length > 1){ // if there's multiple elements selected
+					return recup;
+				}
+			}else if(arg[0] === '#'){ // If the selection is by string of an id
 				arg = arg.substr(1);
 				recup = document.getElementById(arg);
-				return this;
-			}else if(tags.test(arg)){
+			}else if(tags.test(arg)){  // If the selection is by string of an html tag
 				recup = document.getElementsByTagName(arg);
-				return this; 
+				if(recup.length === 1){  // if there's only one element selected
+					recup = recup[0];
+				}else if(recup.length > 1){  // if there's multiple elements selected
+					return recup;
+				}
 			}else{
 				return undefined;
 			}
+
+		}else if(typeof arg === 'object'){ // If the selection is a DOM object
+			recup = arg;
 		}
+		return this;
+	}
+
+	function embed(attr, prop){
+		if(embeds.test(attr)){
+			if(attr === 'src'){
+				recup.src = prop;
+			}
+		}if(embeds.test(attr)){
+			if(attr === 'href'){
+				recup.href = prop;
+			}
+		}
+		return this;
 	}
 
 	function style(attr, prop){
@@ -30,6 +55,21 @@ var nono = (function(){
 				return this;
 			}else if(attr === 'background-color'){
 				recup.style.backgroundColor = prop;
+				return this;
+			}else if(attr === 'text-decoration'){
+				recup.style.textDecoration = prop;
+				return this;
+			}else if(attr === 'font-style'){
+				recup.style.fontStyle = prop;
+				return this;
+			}else if(attr === 'border'){
+				recup.style.border = prop;
+				return this;
+			}else if(attr === 'width'){
+				recup.style.width = prop;
+				return this;
+			}else if(attr === 'height'){
+				recup.style.height = prop;
 				return this;
 			}else if(attr === 'font-size'){
 				if(typeof prop === 'number'){
@@ -42,7 +82,7 @@ var nono = (function(){
 		}
 	}
 
-	function createjs(tag){
+	function create(tag){
 		if(typeof tag === 'string'){
 			if(tags.test(tag)){
 				recup = document.createElement(tag);
@@ -53,9 +93,32 @@ var nono = (function(){
 		}
 	}
 
+	function display(obj, msg){
+		if(typeof obj === 'object'){
+			obj.innerText = msg;
+			obj.textContent = msg;
+		}else{
+			recup.innerText = arguments[0];
+			recup.textContent = arguments[0];
+		}
+		return this;
+	}
+
+	function stick(){
+		if(arguments.length === 1){
+			document.body.appendChild(arguments[0]);
+		}else if(arguments.length === 2){
+			arguments[0].appendChild(arguments[1]);
+		}
+		return this;
+	}
+
 	return{
-		select : selection,
-		css : style,
-		createjs : createjs
+		select  : selection,
+		css     : style,
+		embed   : embed,
+		create  : create,
+		display : display,
+		stickTo : stick
 	};
 })();
