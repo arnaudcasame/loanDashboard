286f170c7ee8e901704982c4cd999f11587823ae
nono tize kod <djcase001@gmail.com>
2016-07-22 01:01:25 -0400

    Adds the AJAX module and implements the quotes request from the quotes.json file

---
+++ js/ajax.js
@@ -0,0 +1,41 @@
+var quotesJsonFileUrl = 'js/quotes.json';
+var activeQuote;
+var authorPlace = document.getElementById('author');
+var quotePlace = document.getElementById('quote');
+
+
+function request(fileUrl){
+		var xhr, data;
+		if(window.XMLHttpRequest){
+			xhr = new XMLHttpRequest();
+		}else {
+			xhr = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhr.open("GET", fileUrl);
+		xhr.onreadystatechange = function(){
+			if((xhr.readyState == 4) && (xhr.status == 200)){
+				data = JSON.parse(xhr.responseText);
+				if(data[0].hasOwnProperty('author')){
+					treatQuotes(data);
+				}
+			}else if(xhr.readyState < 4){}
+		};
+		xhr.send(null);
+}
+
+
+request(quotesJsonFileUrl);
+//request(gradientsJsonFileUrl);
+
+function treatQuotes(data){
+	var len = data.length;
+	function repeat(){
+		var tirage = Math.floor(Math.random() * ((len-1) - 0 + 1) + 0);
+		authorPlace.textContent = '- '+ data[tirage].author;
+		authorPlace.innerText = '- '+ data[tirage].author;
+		quotePlace.innerText = data[tirage].quote;
+		quotePlace.textContent = data[tirage].quote;
+	}
+	setInterval(repeat, 10000);
+	repeat();
+}
