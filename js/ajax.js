af2f87bd336bc248c5d3718d57a889c70926dded
nono tize kod <djcase001@gmail.com>
2016-07-30 01:18:33 -0400

    Optimizes the request function and implements the treatCurrency function which displays currency rate in the currency section

--- js/ajax.js
+++ js/ajax.js
@@ -2,6 +2,11 @@ var quotesJsonFileUrl = 'js/quotes.json';
 var authorPlace = document.getElementById('author');
 var quotePlace = document.getElementById('quote');
 var quoteContain = document.getElementById('quotes');
+var currlayer = 'http://apilayer.net/api/live?access_key=a3bc71915116d84f1765f34ac76a82b7';
+var currlayer2 = 'js/currencylayer.json';
+var gourde = document.getElementById('gourde');
+var euro = document.getElementById('euro');
+var canada = document.getElementById('canada');
 
 
 function request(fileUrl){
@@ -13,10 +18,17 @@ function request(fileUrl){
 		}
 		xhr.open("GET", fileUrl);
 		xhr.onreadystatechange = function(){
+				//console.log(xhr.status);
 			if((xhr.readyState == 4) && (xhr.status == 200)){
 				data = JSON.parse(xhr.responseText);
-				if(data[0].hasOwnProperty('author')){
-					treatQuotes(data);
+				//console.log(typeof data);
+				if(Array.isArray(data)){
+					if(data[0].hasOwnProperty('author')){
+						treatQuotes(data);
+					}
+				}else if(typeof data === 'object'){
+					//console.log(data);
+					treatCurrency(data);
 				}
 			}else if(xhr.readyState < 4){}
 		};
@@ -25,6 +37,7 @@ function request(fileUrl){
 
 
 request(quotesJsonFileUrl);
+request(currlayer2);
 
 function treatQuotes(data){
 	var len = data.length;	
@@ -54,4 +67,15 @@ function treatQuotes(data){
 	}
 	setInterval(repeat, 10000);
 	repeat();
+}
+
+function treatCurrency(data){
+	gourde.innerText = arrondi(data.quotes.USDHTG, 2)+' HTG';
+	euro.innerText = arrondi(data.quotes.USDEUR, 2);
+	canada.innerText = arrondi(data.quotes.USDCAD, 2)+' CAD';
+	gourde.textContent = arrondi(data.quotes.USDHTG, 2)+' HTG';
+	euro.textContent = arrondi(data.quotes.USDEUR, 2);
+	canada.textContent = arrondi(data.quotes.USDCAD, 2)+' CAD';
+	var jourdhui = new Date(data.timestamp * 1000);
+	console.log(jourdhui.toString());
 }
