df16bbf24cf80dd49e70fc6caa0a88178b2bf9ec
nono tize kod <djcase001@gmail.com>
2016-09-10 06:59:59 -0400

    Adds a new javascript file to manage the ui interaction

---
+++ js/uiInteract.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	document.getElementById('_1').checked = false;
+	document.getElementById('_2').checked = false;
+	document.getElementById('_12').checked = true;
+	var etiquettes = $('.label-text');
+	var fields = $('#montant, #taux, #duree');
+	fields.val('');
+	
+	fields.focus(function(){
+		$(this).parent().children('.label-text').addClass('label-text-top');
+	});
+
+	fields.focusout(function() {
+		if ($.trim($(this).val()).length == 0){
+			$(this).parent().children('.label-text').removeClass('label-text-top');
+		}
+		/*
+		$('div#formWrapper').removeClass('darken-bg');
+		$('div.logo').removeClass('logo-active');
+		*/
+	});
+});
